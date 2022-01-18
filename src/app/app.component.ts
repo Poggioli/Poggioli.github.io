edbfb7b953f57b01e0d935407041dd7ef9fc729e
Joao Pogiolli <joaopogiolli@gmail.com>
2022-01-18 15:04:43 -0300

    Commit inicial do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poggi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Poggioli';
+}
